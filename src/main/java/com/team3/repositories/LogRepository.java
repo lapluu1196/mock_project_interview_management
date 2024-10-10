0398fc32ed8991c50d01810575c3459f365c96da
Dinh Lap <lapld.qtkd@gmail.com>
2024-10-10 13:28:08 +0700

    Create repositories: user, candidate, job, interview, offer, log

---
+++ src/main/java/com/team3/repositories/LogRepository.java
@@ -0,0 +1,9 @@
+package com.team3.repositories;
+
+import com.team3.entities.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogRepository extends JpaRepository<Log, Long> {
+}
