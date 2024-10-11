235e17ede83375f73de6c2465774f89fec6d4dda
Long Le <longlehoang119@gmail.com>
2024-10-11 20:29:06 +0700

    Update CandidateService

--- src/main/java/com/team3/services/CandidateService.java
+++ src/main/java/com/team3/services/CandidateService.java
@@ -1,4 +1,17 @@
 package com.team3.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.team3.entities.Candidate;
+import com.team3.repositories.CandidateRepository;
+
+@Service
 public interface CandidateService {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public List<Candidate> getAllCandidates() {
+        return candidateRepository.findAll();
 }
