a59156007e09a32a603f76939d2c459fccdfd20e
Dinh Lap <lapld.qtkd@gmail.com>
2024-10-13 22:36:43 +0700

    UC34: view user detail

--- src/main/java/com/team3/services/CandidateService.java
+++ src/main/java/com/team3/services/CandidateService.java
@@ -2,9 +2,9 @@ package com.team3.services;
 
 import java.util.List;
 
-//import com.team3.dtos.candidate.CandidateDTO;
+import com.team3.dtos.candidate.CandidateDTO;
 
 public interface CandidateService {
     // Search for candidates based on keyword and status
-//    List<CandidateDTO> searchCandidates(String keyword, String status);
+    List<CandidateDTO> searchCandidates(String keyword, String status);
 }
