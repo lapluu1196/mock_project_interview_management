f3557593d1739b907acdfd13cdc405ee690ec054
Dinh Lap <lapld.qtkd@gmail.com>
2024-10-13 11:36:26 +0700

    Revert "Update CandidateService"
    This reverts commit 235e17ede83375f73de6c2465774f89fec6d4dda.

--- src/main/java/com/team3/services/CandidateService.java
+++ src/main/java/com/team3/services/CandidateService.java
@@ -1,17 +1,4 @@
 package com.team3.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.team3.entities.Candidate;
-import com.team3.repositories.CandidateRepository;
-
-@Service
 public interface CandidateService {
-
-    @Autowired
-    private CandidateRepository candidateRepository;
-
-    public List<Candidate> getAllCandidates() {
-        return candidateRepository.findAll();
 }
