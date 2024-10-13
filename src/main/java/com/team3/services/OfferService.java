431443dc3f864043e202c6dd0527422e96a24c6d
Tuan Minh <ngtuanminh238@gmail.com>
2024-10-14 00:53:55 +0700

    Update offer-list view

--- src/main/java/com/team3/services/OfferService.java
+++ src/main/java/com/team3/services/OfferService.java
@@ -1,4 +1,9 @@
 package com.team3.services;
 
+import com.team3.entities.Offer;
+
+import java.util.List;
+
 public interface OfferService {
+    List<Offer> getAllOffers();
 }
--- src/main/java/com/team3/services/impl/OfferServiceImpl.java
+++ src/main/java/com/team3/services/impl/OfferServiceImpl.java
@@ -1,8 +1,21 @@
 package com.team3.services.impl;
 
+import com.team3.entities.Offer;
+import com.team3.repositories.OfferRepository;
 import com.team3.services.OfferService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OfferServiceImpl implements OfferService {
+
+    @Autowired
+    private OfferRepository offerRepository;
+
+    @Override
+    public List<Offer> getAllOffers() {
+        return offerRepository.findAll();
+    }
 }
