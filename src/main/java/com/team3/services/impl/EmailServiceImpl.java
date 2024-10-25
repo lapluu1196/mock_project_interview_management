4e71d644ebaf79abb2aa66126ce4effa6b0d535b
Lap Luu <63952696+lapluu1196@users.noreply.github.com>
2024-10-25 20:29:04 +0700

    Update EmailServiceImpl.java

--- src/main/java/com/team3/services/impl/EmailServiceImpl.java
+++ src/main/java/com/team3/services/impl/EmailServiceImpl.java
@@ -1,7 +1,11 @@
 package com.team3.services.impl;
 
-import com.team3.dtos.user.EmailDTO;
+import com.team3.dtos.email.EmailDTO;
 import com.team3.services.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -10,37 +14,37 @@ import java.util.Map;
 
 @Service
 public class EmailServiceImpl implements EmailService {
-    // private final JavaMailSender javaMailSender;
-    //private final TemplateEngine templateEngine;
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
 
-//    public EmailServiceImpl(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
-//        this.javaMailSender = javaMailSender;
-//        this.templateEngine = templateEngine;
-//    }
+    public EmailServiceImpl(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
+        this.javaMailSender = javaMailSender;
+        this.templateEngine = templateEngine;
+    }
 
 
     @Override
     public String sendEmail(EmailDTO emailDTO, String templateName) {
-        return "Hi";
-//        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-//
-//        try {
-//            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-//            messageHelper.setTo(emailDTO.getTo());
-//            messageHelper.setSubject(emailDTO.getSubject());
-//            messageHelper.setFrom(emailDTO.getFrom());
-//
-//            Context context = new Context();
-//            context.setVariables((Map<String, Object>) emailDTO.getData());
-//
-//            String htmlContent = templateEngine.process(templateName, context);
-//            messageHelper.setText(htmlContent, true);
-//
-//            javaMailSender.send(mimeMessage);
-//            return "Email sent successfully!";
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//            return "Error sending email: " + e.getMessage();
-//        }
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            messageHelper.setTo(emailDTO.getTo());
+            messageHelper.setSubject(emailDTO.getSubject());
+            messageHelper.setFrom(emailDTO.getFrom());
+
+            Context context = new Context();
+            context.setVariables((Map<String, Object>) emailDTO.getData());
+
+            String htmlContent = templateEngine.process(templateName, context);
+            messageHelper.setText(htmlContent, true);
+
+            javaMailSender.send(mimeMessage);
+            return "Email sent successfully!";
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return "Error sending email: " + e.getMessage();
+        }
     }
 }
