d74e9c5b429ef5c4ffdac9836bc35c0d7813d46e
Dinh Lap <lapld.qtkd@gmail.com>
2024-10-29 09:58:26 +0700

    Config error page

--- src/main/java/com/team3/controllers/error/ErrorController.java
+++ src/main/java/com/team3/controllers/error/ErrorController.java
@@ -2,8 +2,10 @@ package com.team3.controllers.error;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/error")
 public class ErrorController {
 
     @GetMapping("/error-400")
