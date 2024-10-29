4e5efd1adc4c8bef909346361009534bb8e974ec
Dinh Lap <lapld.qtkd@gmail.com>
2024-10-29 10:15:01 +0700

    Config error page

--- src/main/java/com/team3/configs/CustomErrorPageConfig.java
+++ src/main/java/com/team3/configs/CustomErrorPageConfig.java
@@ -13,11 +13,11 @@ public class CustomErrorPageConfig {
     @Bean
     public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
         return factory -> {
-            factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error-404"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-500"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/error-400"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/error-401"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/error-403"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/error-404"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/error-500"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/error/error-400"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/error/error-401"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/error/error-403"));
         };
     }
 }
